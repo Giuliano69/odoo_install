b6c1a289d7c19ddcf96f00dc639e93cd4f494118
Giuliano69 <giuliano.lotta@gmail.com>
2025-03-08 15:12:13 +0100

    Add files via upload

---
+++ full_accounting_activation/README.rst
@@ -0,0 +1,3 @@
+This module enable admin user to automatically get full accounting feature.
+
+Originally develloped from https://gitlab.com/PNLUG/Odoo/repository/iso_addons
---
+++ full_accounting_activation/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019-2021 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Full Accounting Activation',
+    'summary': 'Base module to enable full accounting feature for admin user',
+    'version': '14.0.1.0.0',
+    'category': 'Hidden',
+    'author': "Sergio Zanchetta",
+    'website': 'https://gitlab.com/PNLUG/Odoo/repository/iso_addons',
+    'license': 'AGPL-3',
+    "depends": [
+        'account',
+    ],
+    "data": [
+        'data/account_default.xml',
+    ],
+    'installable': True,
+}
